687e40c0b9f2c1d78dc048b69b075b90d5c16294
shivamg829 <shivamgangwar2197@gmail.com>
2026-05-20 16:24:32 +0530

    Added JWT middleware and protected profile routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -82,7 +82,24 @@ const loginUser = async (req, res) => {
     });
   }
 };
+// GET USER PROFILE
+const getUserProfile = async (req, res) => {
+
+    try {
+
+        const user = await User.findById(req.user._id).select("-password");
+
+        res.status(200).json(user);
+
+    } catch(error){
+
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
 module.exports = {
   registerUser,
   loginUser,
+  getUserProfile
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { registerUser } = require("../controllers/authController");
 const { loginUser } = require("../controllers/authController");
+const { getUserProfile } = require("../controllers/authController");
+const protect = require("../middleware/authMiddleware");
 
-// REGISTER ROUTE
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
+
 module.exports = router;
